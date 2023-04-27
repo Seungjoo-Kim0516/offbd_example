2056024a3ad7be8ab91d2c6dee1a0227770ad747
Seungjoo-Kim0516 <85114753+Seungjoo-Kim0516@users.noreply.github.com>
2023-04-27 23:52:49 +0900

    Update offbd_node.cpp

--- offbd_node.cpp
+++ offbd_node.cpp
@@ -9,6 +9,9 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+/*
+mavros_msgs는 MAVROS에서 제공하는 토픽이나 메시지들을 포함하고 있음
+*/
 #include <sensor_msgs/Range.h>
 #include <mavros_msgs/PositionTarget.h>
 #include <geometry_msgs/TwistStamped.h>
@@ -39,7 +42,9 @@ mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
-
+/*
+autopilot의 current state를 콜백함 -> arming이나 offboard 모드로 전환할 때 상태 판별하기 위해 사용
+*/
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "offb_node");
@@ -47,16 +52,22 @@ int main(int argc, char **argv)
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
+    //state를 계속 subscribe하기 위함
+
     // ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
     //         ("mavros/setpoint_position/local", 10);
     ros::Publisher local_pos_pub = nh.advertise<mavros_msgs::PositionTarget>
             ("mavros/setpoint_raw/local", 10);
+    //trajectory를 생성하기 위함 (position, velocity, acceleration 값 전달)
+
     // ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::TwistStamped>
     //         ("mavros/setpoint_velocity/cmd_vel", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
             ("mavros/cmd/arming");
+    //arming mode 전환하기 위해 사용
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
+    // 추후에 offboard 모드로 전환하기 위해 사용
 
     ros::Subscriber ds_sub =nh.subscribe<sensor_msgs::Range>("mavros/distance_sensor/lidar",10,ds_cb);
 
@@ -68,6 +79,9 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
     }
+    // mavros와 px4_sitl 연결을 위해 기다리는 시간.
+    // 연결 되면 위에 선언한 토픽들을 publish하고 subscribe할 것임을 전달하고 해당 while문은 종료됨.
+    
     // 1. target point (3,4,5)
 
     // geometry_msgs::PoseStamped pose;
@@ -125,6 +139,7 @@ int main(int argc, char **argv)
     msg.acceleration_or_force.x=0;
     msg.acceleration_or_force.y=0;
     msg.acceleration_or_force.z=0;
+    //미리 Positiontarget 메시지를 initialize해둠 
 
     //4. cylinder trajectory
     // mavros_msgs::PositionTarget msg;
@@ -149,29 +164,31 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
     }
+    //offboard 모드 전환하기 전에 미리 Positiontarget 정보를 보내고 있어야 함
+    //그렇지 않으면 전환이 되지 않음
 
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
-
+    //offboard 모드 전환
     mavros_msgs::CommandBool arm_cmd;
     arm_cmd.request.value = true;
-
+    //arming 모드 전환
     ros::Time last_request = ros::Time::now();
 
-    while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
-                ROS_INFO("Offboard enabled");
+    while(ros::ok()){//ros가 돌아가고 있을 때
+        if( current_state.mode != "OFFBOARD" && //iris(모델)이 offboard 모드가 아니고
+            (ros::Time::now() - last_request > ros::Duration(5.0))){ //ros에서 명령을 전달한 지 5초가 넘었다면
+            if( set_mode_client.call(offb_set_mode) && //iris(모델)을 offboard 모드로 전환하라는 명령을 전달하고
+                offb_set_mode.response.mode_sent){//offboard 명령이 전달 됐다면
+                ROS_INFO("Offboard enabled");//offboard 모드로 전환해라
             }
             last_request = ros::Time::now();
-        } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
-                    ROS_INFO("Vehicle armed");
+        } else { //offboard로 전환됐고
+            if( !current_state.armed && //iris(모델)이 arming 상태가 아니고
+                (ros::Time::now() - last_request > ros::Duration(5.0))){// ros에서 명령을 전달한지 5초가 넘었다면
+                if( arming_client.call(arm_cmd) && //iris(모델)을 arming시키고
+                    arm_cmd.response.success){ //arming 명령이 전달됐다면
+                    ROS_INFO("Vehicle armed");//arming 시켜라
                 }
                 last_request = ros::Time::now();
             }
@@ -241,8 +258,12 @@ int main(int argc, char **argv)
         count++;
         //local_pos_pub.publish(pose);
         local_pos_pub.publish(msg);
-        ros::spinOnce();
-        rate.sleep();
+        ros::spinOnce(); //노드가 꺼지기 전까지 ROS에게 대기 요청하고 콜백을 처리하고 넘어감
+                        //쉽게 말하면 ctrl+c 이전까지 콜백 함수 호출하는 것
+                        // 여기서 말하는 콜백 함수는 state_cb, 드론의 상태를 계속 받아옴
+        rate.sleep(); //while문에서 설정한 주기를 맞추기 위해 기다리는 함수
+                        //위의 ros::Rate rate(20.0);가 그 주기
+                        //콜백 함수에 들어갔다가 다시 나와서 메인 루프가 20hz로 동작하게끔 하는 것
     }
 
     return 0;
